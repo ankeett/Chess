0f3c826c80be4cff0407fae8ffb97763d29625e2
ankeett <94247776+ankeett@users.noreply.github.com>
2024-03-24 11:47:42 -0400

    hosting5

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://thinknmove.netlify.app/"] // Assuming this is your frontend URL
+        origin: ["http://localhost:5173", "https://thinknmove.netlify.app"] // Assuming this is your frontend URL
     }
 });
 
